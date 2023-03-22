a64e7090da9a1ca761f075b85b3e40a02f4f22cd
ricqCodes <princenwakanma1996@gmail.com>
2023-03-22 13:43:27 +0100

    First Commit

---
+++ bid.js
@@ -0,0 +1,88 @@
+global.Headers = global.Headers || require("node-fetch").Headers;
+const opensea = require("opensea-js");
+require("dotenv").config();
+const OpenSeaPort = opensea.OpenSeaPort;
+const Network = opensea.Network;
+const {
+  RPCSubprovider,
+  PrivateKeyWalletSubprovider,
+  Web3ProviderEngine,
+} = require("@0x/subproviders");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const INFURA_KEY = process.env.INFURA_API_KEY;
+const TARGET_CONTRACT_ADDRESS = process.env.TOKEN_CONTRACT_ADDRESS;
+const MY_ADDRESS = process.env.MY_ADDRESS;
+const NETWORK = process.env.NETWORK;
+// const API_KEY = process.env.OPENSEA_API_KEY;
+const API_KEY = "";
+
+// get the command line arguments
+const args = process.argv.slice(2);
+
+if (
+  !PRIVATE_KEY ||
+  !INFURA_KEY ||
+  !NETWORK ||
+  !MY_ADDRESS ||
+  !TARGET_CONTRACT_ADDRESS
+) {
+  console.error(
+    "Please set a mnemonic, infura key, user address, network, and target NFT contract address."
+  );
+  return;
+}
+
+const privateKeyWalletSubprovider = new PrivateKeyWalletSubprovider(
+  PRIVATE_KEY
+);
+
+const infuraRpcSubprovider = new RPCSubprovider(
+  "https://" + NETWORK + ".infura.io/v3/" + INFURA_KEY
+);
+
+const providerEngine = new Web3ProviderEngine();
+providerEngine.addProvider(privateKeyWalletSubprovider);
+providerEngine.addProvider(infuraRpcSubprovider);
+providerEngine.start();
+
+const provider = new HDWalletProvider({
+  privateKeys: [PRIVATE_KEY],
+  providerOrUrl: "https://" + NETWORK + ".infura.io/v3/" + INFURA_KEY,
+  addressIndex: 0,
+});
+
+// Initialize the seaport.
+const seaport = new OpenSeaPort(
+  provider,
+  {
+    networkName: NETWORK === "mainnet" ? Network.Main : Network.Goerli,
+    apiKey: API_KEY,
+  },
+  (arg) => console.log(arg)
+);
+
+async function main() {
+  const tokenId = args[0];
+
+  // Make a bid on an item.
+  console.log(
+    `Bidding on https://testnets.opensea.io/assets/${TARGET_CONTRACT_ADDRESS}/${tokenId}`
+  );
+  try {
+    const offer = await seaport.createBuyOrder({
+      asset: {
+        tokenId,
+        tokenAddress: TARGET_CONTRACT_ADDRESS,
+      },
+      startAmount: args[1],
+      accountAddress: MY_ADDRESS,
+    });
+    console.log("Successfully created a buy order!");
+  } catch (e) {
+    console.log("Failed:", e);
+  }
+}
+
+main().then(() => process.exit());
